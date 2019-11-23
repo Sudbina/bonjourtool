dbe700e471f9684926b354c2607a312dde426fd4
Jack <jack@onebigcircle.co.uk>
2019-11-23 20:24:33 +0000

    add some sort of UI persistant personalisation in settings screen
    probably isn't the most efficient way to do this, more of a proof of concept

--- src/renderer/components/Settings.vue
+++ src/renderer/components/Settings.vue
@@ -1,34 +1,89 @@
 <template>
-  <div class="ebonjour-wrapper" v-anime="{opacity: 1, duration: 1000}">
-      <div class="navigator">
-    <a-button type="primary" style="font-weight: bold;" @click="handleNavigation('browser')"
-      >Back</a-button
-    >
+  <div class="ebonjour-wrapper" :style="`background: ${themeBackground}; color: ${themeText}`" v-anime="{ opacity: 1, duration: 1000 }">
+    <div class="navigator">
+      <a-button
+        type="primary"
+        style="font-weight: bold;"
+        @click="handleNavigation('browser')"
+        >Back</a-button
+      >
+    </div>
+    <div class="settings-container">
+      <p style="font-size: 18px; font-weight: bold;">Settings</p>
+      <div class="settings-inner">
+        <div class="settings-option">
+          <p>Theme:</p>
+          <a-select :defaultValue="this.$cookies.get('userThemePref')" @change="handleTheme">
+            <a-select-option value="light">Light</a-select-option>
+            <a-select-option value="dark">Dark</a-select-option>
+          </a-select>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
+import store from "../store";
+
 export default {
   data() {
-    return {
-    };
+    return {};
+  },
+  mounted() {
+    console.log(this.$cookies.get("userThemePref"));
+  },
+  computed: {
+    themeBackground() {
+      switch(store.state.userThemeSelection){
+        default:
+        case "light":
+          return '#FFFFFF';
+        case "dark":
+          return '#1b1b1b';
+      }
+    },
+    themeText() {
+      switch(store.state.userThemeSelection){
+        default:
+        case "light":
+          return '#1b1b1b';
+        case "dark":
+          return '#FFFFFF';
+      }
+    }
   },
   methods: {
     handleNavigation(screen) {
       this.$router.replace(screen);
+    },
+    handleTheme(theme) {
+      console.log("[VX] CHANGE THEME: ", theme);
+      store.dispatch("handleUserThemeChange", theme).then((success, error) => {
+          this.$cookies.set("userThemePref", theme);
+          console.log(this.$cookies.get("userThemePref"));
+      });
     }
   }
 };
 </script>
 
-<style>
+<style
+  v-if="
+    state.userThemeSelection === null || state.userThemeSelection === 'light'
+  "
+  lang="scss"
+>
+p {
+  margin: 0px !important;
+}
 .ebonjour-wrapper {
   display: flex;
   height: 100%;
   overflow: scroll;
   max-width: 100%;
   background: #efefef;
+  transition: 0.3s ease-in-out;
 }
 .navigator {
   position: absolute;
@@ -39,5 +94,21 @@ export default {
   padding: 10px;
   background: white;
   box-shadow: 0px -3px 13px 0px rgba(0, 0, 0, 0.15);
+}
+.settings-container {
+  width: 100%;
+  padding: 10px;
+  .settings-inner {
+    display: flex;
+    flex-direction: column;
+    margin-left: 15px;
+    .settings-option {
+      width: 200px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 5px 0px; //tb lr
+    }
+  }
 }
 </style>
--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,17 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
-import { createPersistedState, createSharedMutations } from 'vuex-electron'
+import VueCookies from 'vue-cookies';
 
 import modules from './modules'
 
 Vue.use(Vuex)
+Vue.use(VueCookies);
 
 export default new Vuex.Store({
+  state: {
+    userThemeSelection: VueCookies.get('userThemePref') ? VueCookies.get('userThemePref') : null
+  },
+  mutations: {
+    handleUserThemeChange(state, value) {
+      state.userThemeSelection = value;
+    }
+  },
+  actions: {
+    handleUserThemeChange(context, value) {
+      context.commit('handleUserThemeChange', value);
+    }
+  },
   modules,
-  plugins: [
-    createPersistedState(),
-    createSharedMutations()
-  ],
   strict: process.env.NODE_ENV !== 'production'
 })
